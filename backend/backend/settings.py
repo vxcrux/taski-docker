9a420fdf5dc234434c1e3fad57f3449a2db0fc3a
Maksim Andrianov <maxalexvxcrux@icloud.com>
2026-04-26 22:28:17 +0300

    2122

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '<your_domain>']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '123.123.123.123', 'hbudhdbuh.servepics.com']
 
 
 # Application definition
